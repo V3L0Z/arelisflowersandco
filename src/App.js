db45c2bee6a7778137e6d240aa68017e5c1f806c
Areli Tirado <arelitirado@Arelis-MBP.socal.rr.com>
2022-01-10 19:07:34 -0800

    added flowers and menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation logo={logo} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ed <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Areli's Flowers And Co.
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
